d0e3ed5b573a32be14370e59fc81f58e9e729966
DOliveiira <m.danilo.oliveira@hotmail.com>
2024-03-31 17:14:12 -0300

    Adicionado o filtro de Especies

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const charsConteiner = document.querySelector('.chars-container');
+const speciesFielter = document.querySelector('#species')
 
 const API = 'https://rickandmortyapi.com/api'
 const defaultFilters = {
@@ -18,7 +19,6 @@ async function getCharacters({ name, species, gender, status, page = 1 }) {
 
 async function render(characters){
     characters.forEach((character) => {
-
         return charsConteiner.innerHTML += `
         <div class="char">
             <img src="${character.image}" alt="">
@@ -28,9 +28,15 @@ async function render(characters){
             </div>
          </div>
         `
-
     })
 }
+
+speciesFielter.addEventListener('change', async (event) => {
+    defaultFilters.species = event.target.value
+    charsConteiner.innerHTML = ''
+    const characters = await getCharacters(defaultFilters)
+    render(characters)
+})
 
 async function main(){
     const characters = await getCharacters(defaultFilters)
